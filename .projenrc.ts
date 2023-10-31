032fea45a9d14de314aa3f673e0ac73521f2d82b
Lora Reames <shes.lora.reames@gmail.com>
2023-10-31 13:04:08 -0500

    empty push to locate repo

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,6 @@ const project = new typescript.TypeScriptAppProject({
   defaultReleaseBranch: 'main',
   name: 'TimelinesPlugin',
   projenrcTs: true,
-
   // deps: [],                /* Runtime dependencies of this module. */
   // description: "Timeline-ify your Discord, add filter tags and custom timelines.",  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
