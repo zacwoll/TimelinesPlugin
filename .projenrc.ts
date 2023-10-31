392afc1e99e25f57bd646ac15b199dd20f66e6ea
Lora Reames <shes.lora.reames@gmail.com>
2023-10-31 13:01:47 -0500

    used projenrc and generated filers

--- .projenrc.ts
+++ .projenrc.ts
@@ -5,7 +5,7 @@ const project = new typescript.TypeScriptAppProject({
   projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // description: "Timeline-ify your Discord, add filter tags and custom timelines.",  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
